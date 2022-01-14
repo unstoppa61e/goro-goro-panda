927968d2f2dc2e906f6d166cef5e011c146946f4
Makoto Onoue <like_a_newborn_baby@icloud.com>
2022-01-14 16:39:29 +0900

    Vue.jsを導入した

---
+++ app/javascript/hello.vue
@@ -0,0 +1,18 @@
+<template>
+  <h2>
+    {{ hello }}
+  </h2>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      hello: "Hello, Vue.js 3.0"
+    }
+  }
+}
+</script>
+
+<style scoped>
+</style>
