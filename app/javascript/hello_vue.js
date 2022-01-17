00de748d66dcdd15c10dd1771779e5c9ef0c300a
Makoto Onoue <like_a_newborn_baby@icloud.com>
2022-01-17 11:16:49 +0900

    prettierによる修正を適用した

--- app/javascript/hello_vue.js
+++ app/javascript/hello_vue.js
@@ -2,8 +2,8 @@ import { createApp } from 'vue'
 import App from './helloTest.vue'
 
 document.addEventListener('DOMContentLoaded', () => {
-    const selector = '#js-hello-vue';
-    if (document.querySelector(selector)) {
-        createApp(App).mount(selector);
-    }
+  const selector = '#js-hello-vue'
+  if (document.querySelector(selector)) {
+    createApp(App).mount(selector)
+  }
 })
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,9 +3,9 @@
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
 
-import Rails from "@rails/ujs"
-import Turbolinks from "turbolinks"
-import "../hello_vue.js"
+import Rails from '@rails/ujs'
+import Turbolinks from 'turbolinks'
+import '../hello_vue.js'
 
 Rails.start()
 Turbolinks.start()
