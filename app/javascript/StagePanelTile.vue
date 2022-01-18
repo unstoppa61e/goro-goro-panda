f10905b334f02e8b7f3560c4c03d58a3e90d24b3
Makoto Onoue <like_a_newborn_baby@icloud.com>
2022-01-19 08:06:39 +0900

    タイルコンポーネントに親から文字列を渡す処理までを実装

---
+++ app/javascript/StagePanelTile.vue
@@ -0,0 +1,29 @@
+<template>
+  <img :src="imgSrc" class="wordplay-img" alt="wordplay" />
+  <!--  動かない-->
+  <div class="number">{{ number }}</div>
+  <!--  これなら動く-->
+  <!--  <img src="~wordplays/14.png" class="wordplay-img" alt="wordplay" />-->
+</template>
+
+<script>
+export default {
+  props: ['number'],
+  computed: {
+    imgSrc() {
+      return `~wordplays/${this.number}.png`
+    }
+  }
+}
+</script>
+
+<style scoped>
+.wordplay-img {
+  max-width: 100%;
+  max-height: 100%;
+}
+.number {
+  color: #ffffff;
+  font-size: 30px;
+}
+</style>
