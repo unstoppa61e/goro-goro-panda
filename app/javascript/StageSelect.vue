47caaa5be8facb4e5e58bbfa56f0c112851eee4c
unstoppa61e <like_a_newborn_baby@icloud.com>
2022-01-19 13:22:39 +0900

    パネルをコンポーネントとして切り出し、内容が重複した状態でループさせた

--- app/javascript/StageSelect.vue
+++ app/javascript/StageSelect.vue
@@ -1,27 +1,17 @@
 <template>
   <ul class="panels-container">
-    <li class="panel panel-1">
-      <ul class="tiles-container">
-        <li v-for="number in numbers" :key="number" class="tile">
-          <stage-panel-tile :number="number"></stage-panel-tile>
-        </li>
-      </ul>
+    <li v-for="panelNumber in 10" :key="panelNumber">
+      <stage-select-panel :panel-number="panelNumber"></stage-select-panel>
     </li>
   </ul>
 </template>
 
 <script>
-import StagePanelTile from './StagePanelTile.vue'
+import StageSelectPanel from './StageSelectPanel'
 export default {
   components: {
-    'stage-panel-tile': StagePanelTile
-  },
-  data() {
-    return {
-      numbers: ['14', '15', '92', '65', '35']
-    }
-  },
-  methods: {}
+    'stage-select-panel': StageSelectPanel
+  }
 }
 </script>
 
@@ -29,32 +19,14 @@ export default {
 ul {
   padding-inline-start: 0;
 }
-.panel {
-  list-style: none;
-  text-align: center;
-}
-.tiles-container {
-  display: flex;
-  width: 250px;
-}
-.tile {
+li {
   list-style-type: none;
   text-align: center;
 }
-/*.wordplay-img {*/
-/*  max-width: 100%;*/
-/*  max-height: 100%;*/
-/*}*/
-/*.number {*/
-/*  color: #ffffff;*/
-/*  font-size: 30px;*/
-/*}*/
 .panels-container {
   display: flex;
-  justify-content: center;
-}
-.panel-1 {
-  background-color: #ff88bb;
-  border-radius: 5px;
+  flex-direction: column;
+  justify-content: space-between;
+  align-items: center;
 }
 </style>
---
+++ app/javascript/StageSelectPanel.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="panel panel-1">
+    {{ panelNumber }}
+    <ul class="tiles-container">
+      <li v-for="number in numbers" :key="number">
+        <stage-panel-tile :number="number"></stage-panel-tile>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+import StagePanelTile from './StagePanelTile.vue'
+export default {
+  components: {
+    'stage-panel-tile': StagePanelTile
+  },
+  props: {
+    panelNumber: {
+      type: Number,
+      default: null
+    }
+  },
+  data() {
+    return {
+      numbers: ['14', '15', '92', '65', '35']
+    }
+  }
+}
+</script>
+
+<style scoped>
+li {
+  list-style: none;
+  text-align: center;
+}
+.panel {
+  display: flex;
+}
+.panel-1 {
+  background-color: #ff88bb;
+  border-radius: 5px;
+}
+.tiles-container {
+  display: flex;
+  width: 250px;
+}
+</style>
