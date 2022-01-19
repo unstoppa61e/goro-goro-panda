f97a3c19f1f03f38aab763f1bcbc562b9fc83b5a
Makoto Onoue <like_a_newborn_baby@icloud.com>
2022-01-19 12:18:59 +0900

    ESLintを適用

--- app/javascript/stage_panel_tile.js
+++ app/javascript/stage_panel_tile.js
@@ -2,8 +2,8 @@ import { createApp } from 'vue'
 import StagePanelTile from './StagePanelTile.vue'
 
 document.addEventListener('DOMContentLoaded', () => {
-    const selector = '#js-stage-panel-tile'
-    if (document.querySelector(selector)) {
-        createApp(StagePanelTile).mount(selector)
-    }
+  const selector = '#js-stage-panel-tile'
+  if (document.querySelector(selector)) {
+    createApp(StagePanelTile).mount(selector)
+  }
 })
