992e718a722681c996e3365a309e2e8e9098235c
unstoppa61e <like_a_newborn_baby@icloud.com>
2022-02-03 14:43:22 +0900

    Tailwindの適用されるディレクトリを指定した

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,10 @@
 module.exports = {
   mode: 'jit',
   purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
-  content: [],
+  content: [
+    './pages/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}',
+  ],
   theme: {
     extend: {},
   },
