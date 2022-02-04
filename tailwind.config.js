0e9ca6a21bb2f5f5ca072f2b4ad288019193a0e8
unstoppa61e <like_a_newborn_baby@icloud.com>
2022-02-04 10:18:08 +0900

    tailwindの設定が古いバージョン用の記法だったのを修正した

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,30 +1,28 @@
 module.exports = {
-  mode: 'jit',
-  purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
   content: [
-    './pages/**/*.{js,ts,jsx,tsx}',
-    './components/**/*.{js,ts,jsx,tsx}',
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {
       colors: {
-        'unlocked': '#b7d6e8',
-        'stage-1': '#ff88bb',
-        'stage-2': '#bbeeff',
-        'stage-3': '#82eedd',
-        'stage-4': '#de99ff',
-        'stage-5': '#0cbbff',
-        'stage-6': '#ffcc33',
-        'stage-7': '#ff3978',
-        'stage-8': '#ffaa00',
-        'stage-9': '#99ee43',
-        'stage-10': '#1e88ff',
-        'focused': '#fff000',
-        'mistaken': '#ffb5f0',
-        'ok': '#46d2aa',
-        'ng': '#ff3978'
-      }
+        unlocked: "#b7d6e8",
+        "stage-1": "#ff88bb",
+        "stage-2": "#bbeeff",
+        "stage-3": "#82eedd",
+        "stage-4": "#de99ff",
+        "stage-5": "#0cbbff",
+        "stage-6": "#ffcc33",
+        "stage-7": "#ff3978",
+        "stage-8": "#ffaa00",
+        "stage-9": "#99ee43",
+        "stage-10": "#1e88ff",
+        focused: "#fff000",
+        mistaken: "#ffb5f0",
+        ok: "#46d2aa",
+        ng: "#ff3978",
+      },
     },
   },
   plugins: [],
-}
+};
