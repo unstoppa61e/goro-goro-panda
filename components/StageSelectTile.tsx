d7411f3c914b82f5d1f7a0dcba8b09016c21aa2e
unstoppa61e <like_a_newborn_baby@icloud.com>
2022-02-07 11:23:41 +0900

    eslint/prettierを適用させた

--- components/StageSelectTile.tsx
+++ components/StageSelectTile.tsx
@@ -1,14 +1,16 @@
-import Image from "next/image";
+import Image from 'next/image';
 
 type Props = {
   tileNumber: string;
   isLocked: boolean;
 };
-const StageSelectTile = ({ tileNumber, isLocked }: Props) => {
+function StageSelectTile({ tileNumber, isLocked }: Props) {
   const srcPath = (isLocked: boolean, tileNumber: string): string => {
-    if (isLocked) return "/mark_question.png";
+    if (isLocked) return '/mark_question.png';
+
     return `/wordplays/${tileNumber}.png`;
   };
+
   return (
     <>
       <Image
@@ -21,6 +23,6 @@ const StageSelectTile = ({ tileNumber, isLocked }: Props) => {
       <div className="text-center text-white text-3xl">{tileNumber}</div>
     </>
   );
-};
+}
 
 export default StageSelectTile;
