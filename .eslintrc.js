5d41a0b798dc34765bb4294a3820e6e6e07dd550
unstoppa61e <like_a_newborn_baby@icloud.com>
2022-02-07 09:20:37 +0900

    prettierの設定を追加した

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,99 +4,100 @@ module.exports = {
     es2021: true,
   },
   extends: [
-    "plugin:react/recommended",
-    "airbnb",
-    "airbnb/hooks",
-    "plugin:import/errors",
-    "plugin:import/warnings",
-    "plugin:import/typescript",
-    "plugin:@typescript-eslint/recommended",
-    "plugin:@typescript-eslint/recommended-requiring-type-checking",
+    'plugin:react/recommended',
+    'airbnb',
+    'airbnb/hooks',
+    'plugin:import/errors',
+    'plugin:import/warnings',
+    'plugin:import/typescript',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:@typescript-eslint/recommended-requiring-type-checking',
+    'prettier',
   ],
-  parser: "@typescript-eslint/parser",
+  parser: '@typescript-eslint/parser',
   parserOptions: {
     ecmaFeatures: {
       jsx: true,
     },
-    ecmaVersion: "latest",
-    project: "./tsconfig.eslint.json",
-    sourceType: "module",
+    ecmaVersion: 'latest',
+    project: './tsconfig.eslint.json',
+    sourceType: 'module',
     tsconfigRootDir: __dirname,
   },
-  plugins: ["@typescript-eslint", "import", "jsx-a11y", "react", "react-hooks"],
+  plugins: ['@typescript-eslint', 'import', 'jsx-a11y', 'react', 'react-hooks'],
   root: true,
   rules: {
-    "no-use-before-define": "off",
-    "@typescript-eslint/no-use-before-define": ["error"],
-    "lines-between-class-members": [
-      "error",
-      "always",
+    'no-use-before-define': 'off',
+    '@typescript-eslint/no-use-before-define': ['error'],
+    'lines-between-class-members': [
+      'error',
+      'always',
       {
         exceptAfterSingleLine: true,
       },
     ],
-    "no-void": [
-      "error",
+    'no-void': [
+      'error',
       {
         allowAsStatement: true,
       },
     ],
-    "padding-line-between-statements": [
-      "error",
+    'padding-line-between-statements': [
+      'error',
       {
-        blankLine: "always",
-        prev: "*",
-        next: "return",
+        blankLine: 'always',
+        prev: '*',
+        next: 'return',
       },
     ],
-    "@typescript-eslint/no-unused-vars": [
-      "error",
+    '@typescript-eslint/no-unused-vars': [
+      'error',
       {
-        vars: "all",
-        args: "after-used",
-        argsIgnorePattern: "_",
+        vars: 'all',
+        args: 'after-used',
+        argsIgnorePattern: '_',
         ignoreRestSiblings: false,
-        varsIgnorePattern: "_",
+        varsIgnorePattern: '_',
       },
     ],
-    "import/extensions": [
-      "error",
-      "ignorePackages",
+    'import/extensions': [
+      'error',
+      'ignorePackages',
       {
-        js: "never",
-        jsx: "never",
-        ts: "never",
-        tsx: "never",
+        js: 'never',
+        jsx: 'never',
+        ts: 'never',
+        tsx: 'never',
       },
     ],
-    "react/jsx-filename-extension": [
-      "error",
+    'react/jsx-filename-extension': [
+      'error',
       {
-        extensions: [".jsx", ".tsx"],
+        extensions: ['.jsx', '.tsx'],
       },
     ],
-    "react/jsx-props-no-spreading": [
-      "error",
+    'react/jsx-props-no-spreading': [
+      'error',
       {
-        html: "enforce",
-        custom: "enforce",
-        explicitSpread: "ignore",
+        html: 'enforce',
+        custom: 'enforce',
+        explicitSpread: 'ignore',
       },
     ],
-    "react/react-in-jsx-scope": "off",
+    'react/react-in-jsx-scope': 'off',
   },
   overrides: [
     {
-      files: ["*.tsx"],
+      files: ['*.tsx'],
       rules: {
-        "react/prop-types": "off",
+        'react/prop-types': 'off',
       },
     },
   ],
   settings: {
-    "import/resolver": {
+    'import/resolver': {
       node: {
-        paths: ["src"],
+        paths: ['src'],
       },
     },
   },
