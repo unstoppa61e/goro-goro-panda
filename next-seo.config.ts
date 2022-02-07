ab441c116811bed075f7946cde2fddf3d9fe0f33
unstoppa61e <like_a_newborn_baby@icloud.com>
2022-02-08 08:59:52 +0900

    next-seo用のコンフィグを作成した
    コンフィグに設定したものを削除した

---
+++ next-seo.config.ts
@@ -0,0 +1,26 @@
+export default {
+  title: 'ゴロゴロ円周率',
+  description: 'ゲームとゴロ合わせで円周率を覚えよう！',
+  canonical: 'https://gorogoropanda.com/',
+  openGraph: {
+    url: 'https://gorogoropanda.com',
+    title: 'ゴロゴロ円周率',
+    description: 'ゲームとゴロ合わせで円周率を覚えよう！',
+    images: [
+      {
+        url: 'https://gorogoropanda.com/ogp/ogp.png.',
+        width: 1200,
+        height: 630,
+        alt: 'Og Image Alt',
+        type: 'image/png',
+      },
+    ],
+    site_name: 'ゴロゴロ円周率',
+    type: 'website',
+  },
+  twitter: {
+    handle: '@unstoppa61e',
+    site: '@unstoppa61e',
+    cardType: 'summary_large_image',
+  },
+};
