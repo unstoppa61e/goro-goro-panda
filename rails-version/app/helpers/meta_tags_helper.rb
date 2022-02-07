c8f40d95604ded638030dd6c41e979bf4493bdc2
unstoppa61e <like_a_newborn_baby@icloud.com>
2022-02-08 08:59:52 +0900

    next-seo用のコンフィグを作成した
    コンフィグに設定したものを削除した

--- rails-version/app/helpers/meta_tags_helper.rb
+++ rails-version/app/helpers/meta_tags_helper.rb
@@ -4,26 +4,15 @@ module MetaTagsHelper
   # rubocop:disable Metrics/MethodLength
   def default_meta_tags
     {
-      site: 'ゴロゴロ円周率',
       reverse: true,
       charset: 'utf-8',
-      description: 'ゴロ合わせのゲームで円周率を覚えよう！',
       keywords: '円周率, ゲーム, ゴロ, 語呂, 暗記, 覚える',
       viewport: 'width=device-width, initial-scale=1.0',
       og: {
-        title: :title,
-        type: 'website',
-        site_name: 'ゴロゴロ円周率',
-        description: :description,
-        image: 'http://gorogoropanda.com/ogp/ogp.png',
-        url: 'http://gorogoropanda.com'
       },
       twitter: {
-        card: 'summary',
-        site: '@unstoppa61e',
         description: :description,
         image: 'http://gorogoropanda.com/ogp/ogp.png',
-        domain: 'http://gorogoropanda.com'
       }
     }
   end
