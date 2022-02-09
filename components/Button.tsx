f4661b76cdf14260852b27d7af7d46acae5358cc
unstoppa61e <like_a_newborn_baby@icloud.com>
2022-02-09 12:09:00 +0900

    ボタンの見た目を作成した

---
+++ components/Button.tsx
@@ -0,0 +1,9 @@
+const Button = () => {
+  return (
+    <button className="rounded-lg h-11 w-36 border-2 border-white bg-ok text-lg font-bold">
+      おぼえた！
+    </button>
+  );
+};
+
+export default Button;
