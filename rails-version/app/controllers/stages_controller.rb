4efef836893127e862c942ec159b9e2bde4ee749
unstoppa61e <like_a_newborn_baby@icloud.com>
2022-02-10 10:52:11 +0900

    Rails + Vue.js による旧バージョンを削除

--- rails-version/app/controllers/stages_controller.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-class StagesController < ApplicationController
-  def index; end
-
-  def show
-    @id = params[:id]
-  end
-end
--- rails-version/app/javascript/stage_select.js
+++
@@ -1,8 +0,0 @@
-import { createApp } from 'vue'
-import StageSelect from './StageSelect.vue'
-
-document.addEventListener('DOMContentLoaded', () => {
-  const selector = '#js-stage-select'
-  if (!document.querySelector(selector)) return
-  createApp(StageSelect).mount(selector)
-})
