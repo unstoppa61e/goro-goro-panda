ca23f27524a9162d3414e6f8c178f7131e688963
unstoppa61e <like_a_newborn_baby@icloud.com>
2022-02-10 17:09:14 +0900

    クエスチョンマークのイラストをrotateで見えなくした

--- src/components/WordplayTile.tsx
+++ src/components/WordplayTile.tsx
@@ -33,20 +33,24 @@ const WordplayTile = ({ tilePiNumbers }: Props) => {
   return (
     <div className="flex items-center justify-center h-24 w-16 py-1 rounded bg-focused">
       <div className="h-full w-14 flex flex-col justify-between">
-        <div className="flex justify-center items-center h-14 bg-stage-1 rounded">
-          <Image
-            src={wordplayImageSrc()}
-            width={50}
-            height={50}
-            objectFit="contain"
-            alt="wordplay"
-          />
+        <div className="flex justify-center items-center h-14 bg-stage-1 rounded relative">
+          <div className="absolute flex justify-center items-center">
+            <Image
+              src={wordplayImageSrc()}
+              width={50}
+              height={50}
+              objectFit="contain"
+              alt="wordplay"
+              className="rotate-y-0"
+            />
+          </div>
           <Image
             src="/mark_question.png"
             width={50}
             height={50}
             objectFit="contain"
             alt="wordplay"
+            className="rotate-y-90"
           />
         </div>
         <ul className="flex justify-between">{numberTiles}</ul>
