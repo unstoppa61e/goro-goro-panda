fb888bb1cef1d5d94306c52be676b26b18fe762e
unstoppa61e <like_a_newborn_baby@icloud.com>
2022-02-18 16:50:36 +0900

    どのステージまでクリアしたかをローカルストレージで管理するためのhookを追加した

---
+++ src/hooks/useClearedStage.ts
@@ -0,0 +1,28 @@
+import { useCallback, useEffect, useState } from 'react';
+
+const STORAGE_KEY_CLEARED_STAGE = 'gorogoropanda.com/clearedStage';
+
+export function useClearedStage(
+  defaultValue: number,
+): [clearedStage: string, setClearedStage: (clearedStage: string) => void] {
+  const [clearedStageInternal, setClearedStageInternal] = useState('0');
+
+  useEffect(() => {
+    const storageClearedStage = localStorage.getItem(STORAGE_KEY_CLEARED_STAGE);
+    const clearedStage: string =
+      storageClearedStage === null ? '' : storageClearedStage;
+    if (parseInt(clearedStage) > defaultValue) {
+      setClearedStageInternal(clearedStage);
+    }
+  }, [setClearedStageInternal]);
+
+  const setClearedStage = useCallback(
+    (clearedStage: string) => {
+      localStorage.setItem(STORAGE_KEY_CLEARED_STAGE, clearedStage);
+      setClearedStageInternal(clearedStage);
+    },
+    [setClearedStageInternal],
+  );
+
+  return [clearedStageInternal, setClearedStage];
+}
