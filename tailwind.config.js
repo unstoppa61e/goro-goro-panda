a6e75d43b01a3b60be5f5a6b8f19148fee043a16
unstoppa61e <like_a_newborn_baby@icloud.com>
2022-02-21 16:39:44 +0900

    小杉丸ゴシックフォントを追加した

--- tailwind.config.js
+++ tailwind.config.js
@@ -35,6 +35,9 @@ module.exports = {
         ok: '#46d2aa',
         ng: '#ff3978',
       },
+      fontFamily: {
+        'kosugi-maru': ['Kosugi Maru'],
+      },
     },
   },
   plugins: [rotateY],
