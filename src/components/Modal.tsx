fe022fa58acbc903f404510c1fca243cf90f9b52
unstoppa61e <like_a_newborn_baby@icloud.com>
2022-02-22 11:57:15 +0900

    フレームと祝福の言葉がランダム生成されるようにした

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -9,6 +9,50 @@ type Props = {
 };
 
 const Modal = ({ visible, nextStageNumber }: Props) => {
+  const dice = (count: number): number => {
+    return Math.floor(Math.random() * count);
+  };
+
+  const frameSrc = () => {
+    const framesCount = 7;
+    const frameNumber = dice(framesCount);
+
+    return `/frames/modal_frame_${frameNumber}.png`;
+  };
+
+  const admirationHead = () => {
+    const messages = [
+      'イエーイ！',
+      'うわー！',
+      'ブラボー！',
+      'やったね！',
+      'ワンダフォー！',
+      'わーお！',
+    ];
+
+    return <p>{messages[dice(messages.length)]}</p>;
+  };
+
+  const admirationBody = () => {
+    const messages = [
+      ['がんばっている', 'きみは', 'すてきだよ！'],
+      ['がんばると', 'こんなことも', 'できるんだね！'],
+      ['きみって', '努力家で', 'かっこいいなあ！'],
+      ['きみは', 'がんばるのが', 'とくいなんだね！'],
+      ['きみは', '努力家', 'なんだね！'],
+      ['きみは', 'どんどん', '成長するね！'],
+      ['きみは', 'どんどん', 'のびていくね！'],
+      ['きみの', 'がんばりが', 'かがやいているね！'],
+      ['きみの', 'がんばりに', 'はげまされるよ！'],
+      ['こんなに', 'がんばれるなんて', 'すごいや！'],
+      ['できることが', 'どんどん', '増えていくね！'],
+      ['努力って', 'こんなに', 'すばらしいんだね！'],
+    ];
+    const message = messages[dice(messages.length)];
+
+    return message.map((phrase, index) => <p key={index}>{phrase}</p>);
+  };
+
   return (
     <div
       className={`flex justify-center fixed w-full h-full bg-black bg-opacity-40 duration-100 transition-all z-10 font-kosugi-maru ${
@@ -18,7 +62,7 @@ const Modal = ({ visible, nextStageNumber }: Props) => {
       <div className="flex flex-col items-center absolute top-20 bg-white w-80 h-72 rounded-md">
         <div className="relative h-56">
           <Image
-            src="/modal_frame.png"
+            src={frameSrc()}
             objectFit="contain"
             width={320}
             height={226}
@@ -26,24 +70,24 @@ const Modal = ({ visible, nextStageNumber }: Props) => {
             onContextMenu={(e) => e.preventDefault()}
             onMouseDown={(e) => e.preventDefault()}
           />
-          <div className="flex absolute top-1/2 -translate-y-1/2 left-1/2 -translate-x-1/2">
-            <Image
-              src="/pandas/panda_happy_1.png"
-              objectFit="contain"
-              width={70}
-              height={100}
-              alt="panda"
-              onContextMenu={(e) => e.preventDefault()}
-              onMouseDown={(e) => e.preventDefault()}
-            />
-            <div className="text-base w-48 ml-4">
-              うわー！
-              <br />
-              こんなに
-              <br />
-              がんばれるなんて
-              <br />
-              すごいや！
+          <div className="flex flex-col absolute top-1/2 -translate-y-1/2 left-1/2 -translate-x-1/2">
+            <h1 className="text-base text-center font-bold">
+              🎉クリア、おめでとう！🎉
+            </h1>
+            <div className="flex mt-2">
+              <Image
+                src="/pandas/panda_happy_1.png"
+                objectFit="contain"
+                width={90}
+                height={100}
+                alt="panda"
+                onContextMenu={(e) => e.preventDefault()}
+                onMouseDown={(e) => e.preventDefault()}
+              />
+              <div className="text-sm w-48 ml-4">
+                {admirationHead()}
+                {admirationBody()}
+              </div>
             </div>
           </div>
         </div>
