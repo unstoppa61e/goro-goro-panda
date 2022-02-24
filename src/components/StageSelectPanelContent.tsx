c4f33d83612040de1093343a8ce78086dd46c246
unstoppa61e <like_a_newborn_baby@icloud.com>
2022-02-24 16:12:08 +0900

    ステージ番号にデザインを付けた

--- src/components/StageSelectPanelContent.tsx
+++ src/components/StageSelectPanelContent.tsx
@@ -9,18 +9,21 @@ const StageSelectPanelContent = ({ panelNumber, stage, isLocked }: Props) => {
   const tileNumbers = panelNumber.match(/.{2}/g)!;
 
   return (
-    <>
-      <div className="text-white text-xl mt-7 ml-7 font-varela-round">
-        {stage}
+    <div className="w-full flex items-center px-2">
+      <div className="w-16 flex flex-col items-center">
+        <p className="text-white text-xs scale-75">ステージ</p>
+        <div className="flex h-9 w-9 justify-center items-center text-black text-2xl font-varela-round bg-white rounded-full">
+          {stage}
+        </div>
       </div>
-      <ul className="flex">
+      <ul className="w-full flex justify-between">
         {tileNumbers.map((tileNumber: string, index: number) => (
           <li key={index}>
             <StageSelectTile tileNumber={tileNumber} isLocked={isLocked} />
           </li>
         ))}
       </ul>
-    </>
+    </div>
   );
 };
 
