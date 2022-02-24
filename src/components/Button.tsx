bd807ee1f81e464739a746e0d28a0f01a4946fea
unstoppa61e <like_a_newborn_baby@icloud.com>
2022-02-24 09:54:39 +0900

    「覚えた！」ボタンの一文字目のみが明るく表示されてしまう問題に対処した

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -4,12 +4,15 @@ type Props = {
 
 const Button = ({ handleOnClick }: Props) => {
   return (
-    <button
-      onClick={handleOnClick}
-      className="rounded-lg h-14 w-28 border-2 border-white bg-ok text-lg font-bold mt-8 font-kosugi-maru"
-    >
-      覚えた！
-    </button>
+    <>
+      {/*buttonタグ + 小杉丸ゴシックフォントの組み合わせだと、スマホでのみ、１文字目（「覚」）が他の文字より明るくなるというバグに遭遇したため、divにしてある*/}
+      <div
+        onClick={handleOnClick}
+        className="flex items-center justify-center font-kosugi-maru rounded-lg h-14 w-28 border-2 border-white bg-ok text-lg font-bold mt-8"
+      >
+        覚えた！
+      </div>
+    </>
   );
 };
 
