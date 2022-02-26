78b6df33e3ee2f9df0418f6b421daa56601419b7
unstoppa61e <like_a_newborn_baby@icloud.com>
2022-02-26 12:17:00 +0900

    数字キーをタップした際、縮小し、背景色がグレーになるようにした

--- src/components/numberKey.tsx
+++ src/components/numberKey.tsx
@@ -6,7 +6,7 @@ type Props = {
 const NumberKey = ({ keyNumber, handleInputNumber }: Props) => {
   return (
     <button
-      className="w-14 h-14 rounded-2xl border-4 border-black bg-white text-4xl text-black font-bold"
+      className="w-14 h-14 rounded-2xl border-4 border-black bg-white text-4xl text-black font-bold active:scale-90 active:bg-gray-500"
       onClick={() => handleInputNumber(keyNumber.toString())}
     >
       {keyNumber}
