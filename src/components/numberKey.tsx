89ac8cb2d8e9937f1c3531586ddbd0c336221ccf
unstoppa61e <like_a_newborn_baby@icloud.com>
2022-02-28 12:42:23 +0900

    キーのタップ時の枠線を消去した

--- src/components/numberKey.tsx
+++ src/components/numberKey.tsx
@@ -6,7 +6,7 @@ type Props = {
 const NumberKey = ({ keyNumber, handleInputNumber }: Props) => {
   return (
     <button
-      className="w-16 h-16 flex justify-center items-center"
+      className="w-16 h-16 flex justify-center items-center no-tap-highlighting"
       onClick={() => handleInputNumber(keyNumber.toString())}
     >
       <p className="w-14 h-14 rounded-2xl border-4 border-black bg-white text-4xl text-black font-bold flex justify-center items-center active:scale-90 active:bg-gray-500">
