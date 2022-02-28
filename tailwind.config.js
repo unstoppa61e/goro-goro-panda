21e64103f7fbc12ce9d6296c406fe20b282a2b5c
unstoppa61e <like_a_newborn_baby@icloud.com>
2022-02-28 12:42:23 +0900

    キーのタップ時の枠線を消去した

--- tailwind.config.js
+++ tailwind.config.js
@@ -48,5 +48,14 @@ module.exports = {
       },
     },
   },
-  plugins: [rotateY],
+  plugins: [
+    rotateY,
+    plugin(function ({ addUtilities }) {
+      addUtilities({
+        '.no-tap-highlighting': {
+          '-webkit-tap-highlight-color': 'rgba(0,0,0,0)',
+        },
+      });
+    }),
+  ],
 };
