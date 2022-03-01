f7fc65a169c8d6e71be457f8bc51ee83a19b6e6a
unstoppa61e <like_a_newborn_baby@icloud.com>
2022-03-01 14:58:19 +0900

    数字入力キーにアニメーションを付けた

--- src/components/NumberKey.tsx
+++ src/components/NumberKey.tsx
@@ -9,7 +9,7 @@ const NumberKey = ({ keyNumber, handleInputNumber }: Props) => {
       className="w-16 h-16 flex justify-center items-center no-tap-highlighting cursor-default"
       onClick={() => handleInputNumber(keyNumber.toString())}
     >
-      <p className="w-14 h-14 rounded-2xl border-4 border-black bg-white text-4xl text-black font-bold flex justify-center items-center active:scale-90 active:bg-gray-500 cursor-pointer">
+      <p className="w-14 h-14 rounded-2xl border-4 border-black bg-white text-4xl text-black font-bold flex justify-center items-center active:scale-90 active:bg-gray-500 cursor-pointer animate-pulse animate-infinite">
         {keyNumber}
       </p>
     </button>
