fefb2cc6ff7eab80cccd50196a50ad5f36b99eb7
unstoppa61e <like_a_newborn_baby@icloud.com>
2022-03-01 12:55:53 +0900

    「覚えた！」ボタンにアニメーションを付けた

--- src/components/StartAnsweringButton.tsx
+++ src/components/StartAnsweringButton.tsx
@@ -8,7 +8,7 @@ const StartAnsweringButton = ({ handleOnClick }: Props) => {
       {/*buttonタグ + 小杉丸ゴシックフォントの組み合わせだと、スマホでのみ、１文字目（「覚」）が他の文字より明るくなるというバグに遭遇したため、divにしてある*/}
       <div
         onClick={handleOnClick}
-        className="flex items-center justify-center font-kosugi-maru rounded-lg h-14 w-28 border-2 border-white hover:border-gray-200 hover:text-gray-200 bg-ok text-lg font-bold cursor-pointer"
+        className="flex items-center justify-center font-kosugi-maru rounded-lg h-14 w-28 border-2 border-white hover:border-gray-200 hover:text-gray-200 bg-ok text-lg font-bold cursor-pointer animate-pulse animate-delay-[1800ms] animate-infinite"
       >
         覚えた！
       </div>
