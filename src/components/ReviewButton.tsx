62e5556cef01dcc800eac286ff4ac06472dc77a6
unstoppa61e <like_a_newborn_baby@icloud.com>
2022-03-02 12:49:00 +0900

    hover効果がスマホでの縦画面プレイ時に機能しないようにした

--- src/components/ReviewButton.tsx
+++ src/components/ReviewButton.tsx
@@ -7,7 +7,7 @@ const ReviewButton = ({ handleOnClick }: Props) => {
       {/*buttonタグ + 小杉丸ゴシックフォントの組み合わせだと、スマホでのみ、１文字目（「覚」）が他の文字より明るくなるというバグに遭遇したため、divにしてある*/}
       <div
         onClick={handleOnClick}
-        className="h-12 w-28 cursor-pointer rounded bg-gray-300 hover:bg-gray-500 flex items-center justify-center font-kosugi-maru rounded-lg text-lg font-bold"
+        className="h-12 w-28 cursor-pointer rounded bg-gray-300 sm:hover:bg-gray-500 active:bg-gray-500 flex items-center justify-center font-kosugi-maru rounded-lg text-lg font-bold"
       >
         覚え直す
       </div>
