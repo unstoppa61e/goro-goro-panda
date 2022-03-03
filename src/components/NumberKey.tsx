724933fb3766bc821bf8c0430d3d216e0c5b30b7
unstoppa61e <like_a_newborn_baby@icloud.com>
2022-03-03 13:07:17 +0900

    数字キーにスマホ以外でのプレイ時のみホバーのエフェクトが働くようにした

--- src/components/NumberKey.tsx
+++ src/components/NumberKey.tsx
@@ -13,7 +13,7 @@ const NumberKey = ({ keyNumber, handleInputNumber, mode }: Props) => {
       onClick={() => handleInputNumber(keyNumber.toString())}
     >
       <p
-        className={`w-14 h-14 rounded-2xl border-4 border-black bg-white text-4xl text-black font-bold flex justify-center items-center active:scale-90 active:bg-gray-500 cursor-pointer ${
+        className={`w-14 h-14 rounded-2xl border-4 border-black bg-white text-4xl text-black font-bold flex justify-center items-center active:scale-90 active:bg-gray-500 sm:hover:bg-gray-500 cursor-pointer ${
           mode === MODE.Clear ? '' : 'animate-pulse animate-infinite'
         }`}
       >
