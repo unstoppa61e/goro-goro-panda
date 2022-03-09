d246de7fa5ad44894e406429d14498e2c7ddbeab
unstoppa61e <like_a_newborn_baby@icloud.com>
2022-03-09 12:10:34 +0900

    サイト情報をまとめたファイルを作成した

---
+++ src/lib/site.ts
@@ -0,0 +1,12 @@
+export const Site = {
+  title: 'ゴロゴロ円周率',
+  description: '語呂合わせのゲームで円周率を覚えよう！',
+  origin:
+    process.env.NODE_ENV === 'development'
+      ? 'http://localhost:3000'
+      : 'https://gorogoropanda.com',
+  developer: 'unstoppa61e',
+  note: 'https://note.com/unstoppa61e/n/nd3cdb84b41ad',
+  twitter: 'https://twitter.com/unstoppa61e',
+  github: 'https://github.com/unstoppa61e/goro-goro-panda',
+};
