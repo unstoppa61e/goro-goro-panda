2906dce1edec72098d5d882a0063f0e5686e9c6f
unstoppa61e <like_a_newborn_baby@icloud.com>
2022-03-10 15:22:54 +0900

    まとめステージのクリア履歴管理用のhookを作成した

---
+++ src/hooks/useClearedReview.ts
@@ -0,0 +1,44 @@
+import { useCallback, useEffect, useState } from 'react';
+
+const STORAGE_KEY_CLEARED_REVIEW = 'gorogoropanda.com/clearedReview';
+
+export const clearedReviewDefaultValue = 0;
+
+export const localStorageClearedReviewExists = () => {
+  return localStorage.getItem(STORAGE_KEY_CLEARED_REVIEW) !== null;
+};
+
+export function useClearedReview(
+  defaultValue: number,
+  clearedReviewValues: string[],
+): [clearedReview: number, setClearedReview: (clearedReview: number) => void] {
+  const [clearedReviewInternal, setClearedReviewInternal] = useState(
+    clearedReviewDefaultValue,
+  );
+
+  useEffect(() => {
+    const storageClearedReview = localStorage.getItem(
+      STORAGE_KEY_CLEARED_REVIEW,
+    );
+    const clearedReview: number =
+      storageClearedReview === null
+        ? 0
+        : clearedReviewValues.indexOf(storageClearedReview);
+    if (clearedReview > defaultValue) {
+      setClearedReviewInternal(clearedReview);
+    }
+  }, [setClearedReviewInternal]);
+
+  const setClearedReview = useCallback(
+    (clearedReview: number) => {
+      localStorage.setItem(
+        STORAGE_KEY_CLEARED_REVIEW,
+        clearedReviewValues[clearedReview],
+      );
+      setClearedReviewInternal(clearedReview);
+    },
+    [setClearedReviewInternal],
+  );
+
+  return [clearedReviewInternal, setClearedReview];
+}
