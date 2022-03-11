b88e9d05849c0638e42cc1ed78a038762b28a7d1
unstoppa61e <like_a_newborn_baby@icloud.com>
2022-03-11 09:57:52 +0900

    bodyの背景色のスタイリングの方法を変更した

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -36,7 +36,7 @@ class MyDocument extends Document {
             rel="stylesheet"
           />
         </Head>
-        <body>
+        <body className="bg-theme">
           <Main />
           <NextScript />
         </body>
