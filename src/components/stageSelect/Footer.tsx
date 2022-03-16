632bf6f268bdb82d0e4454cb546ed8966fadb3f7
unstoppa61e <like_a_newborn_baby@icloud.com>
2022-03-16 12:07:28 +0900

    フッターの背景色を無くし、クリック領域を広くした

--- src/components/stageSelect/Footer.tsx
+++ src/components/stageSelect/Footer.tsx
@@ -8,13 +8,13 @@ const Footer = () => {
   const iconSize = 16;
 
   return (
-    <footer className="flex justify-center items-center bg-black h-8 text-white">
-      <div className="flex items-center gap-x-3">
+    <footer className="flex justify-center items-center text-white py-3">
+      <div className="flex items-center gap-x-2.5">
         <p className="text-xs">&copy; 2022 {Site.developer}</p>
-        <div className="flex items-center gap-x-3">
+        <div className="flex items-center">
           <a
             href={Site.note}
-            className="flex justify-center items-center"
+            className="flex justify-center items-center p-2.5"
             target="_blank"
             rel="noopener noreferrer"
           >
@@ -32,12 +32,22 @@ const Footer = () => {
               }
             />
           </a>
-          <a href={Site.twitter} target="_blank" rel="noopener noreferrer">
+          <a
+            href={Site.twitter}
+            target="_blank"
+            rel="noopener noreferrer"
+            className="p-2.5"
+          >
             <IconContext.Provider value={{ size: `${iconSize}px` }}>
               <FaTwitter />
             </IconContext.Provider>
           </a>
-          <a href={Site.github} target="_blank" rel="noopener noreferrer">
+          <a
+            href={Site.github}
+            target="_blank"
+            rel="noopener noreferrer"
+            className="p-2.5"
+          >
             <IconContext.Provider value={{ size: `${iconSize}px` }}>
               <FaGithub />
             </IconContext.Provider>
