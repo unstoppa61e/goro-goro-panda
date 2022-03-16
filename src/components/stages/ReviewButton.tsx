00784e6d5f138c10ed37e0423744488c28b8c8c3
unstoppa61e <like_a_newborn_baby@icloud.com>
2022-03-16 13:22:20 +0900

    「覚えた!」ボタンと「覚え直す」ボタンのデザインをリッチにした

--- src/components/stages/ReviewButton.tsx
+++ src/components/stages/ReviewButton.tsx
@@ -7,8 +7,9 @@ const ReviewButton = ({ handleOnClick }: Props) => {
       {/*buttonタグ + 小杉丸ゴシックフォントの組み合わせだと、スマホでのみ、１文字目（「覚」）が他の文字より明るくなるというバグに遭遇したため、divにしてある*/}
       <div
         onClick={handleOnClick}
-        className="h-12 w-28 cursor-pointer rounded bg-gray-300 sm:hover:bg-gray-500 active:bg-gray-500 flex items-center justify-center font-kosugi-maru rounded-lg text-lg font-bold"
+        className="relative flex items-center justify-center font-kosugi-maru rounded-full py-2.5 px-16 border-4 border-white shadow-lg shadow-black/25 sm:hover:border-gray-200 sm:hover:text-gray-200 active:border-gray-200 active:text-gray-200 bg-gradient-to-b from-navy-darkest to-navy-darker text-lg font-bold cursor-pointer"
       >
+        <span className="absolute top-1.5 w-42 h-5.5 bg-white opacity-30 rounded-full" />
         覚え直す
       </div>
     </>
--- src/components/stages/StartAnsweringButton.tsx
+++ src/components/stages/StartAnsweringButton.tsx
@@ -8,8 +8,9 @@ const StartAnsweringButton = ({ handleOnClick }: Props) => {
       {/*buttonタグ + 小杉丸ゴシックフォントの組み合わせだと、スマホでのみ、１文字目（「覚」）が他の文字より明るくなるというバグに遭遇したため、divにしてある*/}
       <div
         onClick={handleOnClick}
-        className="flex items-center justify-center font-kosugi-maru rounded-lg h-14 w-28 border-2 border-white sm:hover:border-gray-200 sm:hover:text-gray-200 active:border-gray-200 active:text-gray-200 bg-ok text-lg font-bold cursor-pointer animate-pulse animate-delay-[1800ms] animate-infinite"
+        className="relative flex items-center justify-center font-kosugi-maru rounded-full py-2.5 px-16 border-4 border-white shadow-lg shadow-black/25 sm:hover:border-gray-200 sm:hover:text-gray-200 active:border-gray-200 active:text-gray-200 bg-gradient-to-b from-navy-darker to-navy-lightest text-lg font-bold cursor-pointer animate-pulse animate-delay-[1800ms] animate-infinite"
       >
+        <span className="absolute top-1.5 w-42 h-5.5 bg-white opacity-30 rounded-full" />
         覚えた！
       </div>
     </>
