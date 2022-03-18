0fa199c709b3be32a24a24fea76fdfd581e4cf8b
unstoppa61e <like_a_newborn_baby@icloud.com>
2022-03-18 09:05:22 +0900

    ロック中のステージパネルからクエスチョンマークの画像を取り除いた

--- src/components/stageSelect/StageSelectTile.tsx
+++ src/components/stageSelect/StageSelectTile.tsx
@@ -13,27 +13,8 @@ function StageSelectTile({ tileNumber, isLocked, firstTile }: Props) {
 
   const piStartIndicator = <div className="absolute -ml-2">3.</div>;
 
-  const questionImage = (
-    <div className="absolute z-10">
-      <Image
-        src="/mark_question.png"
-        width={50}
-        height={50}
-        objectFit="contain"
-        alt="wordplay"
-        onContextMenu={(e: React.MouseEvent<HTMLImageElement>) =>
-          e.preventDefault()
-        }
-        onMouseDown={(e: React.MouseEvent<HTMLImageElement>) =>
-          e.preventDefault()
-        }
-      />
-    </div>
-  );
-
   const image = (
-    <div className="pointer-events-none relative">
-      {isLocked ? questionImage : null}
+    <div className="pointer-events-none">
       <div className={`${isLocked ? 'brightness-0 invert blur-xs' : ''}`}>
         <Image
           src={srcPath(tileNumber)}
