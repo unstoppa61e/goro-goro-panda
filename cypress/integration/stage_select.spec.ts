220cec79163e5552c6bbf1c23490ffbf05fa22fd
unstoppa61e <like_a_newborn_baby@icloud.com>
2022-03-18 16:41:29 +0900

    各ステージパネルから各ステージへの移動を確認するテストを作成した

--- cypress/integration/stage_select.spec.ts
+++ cypress/integration/stage_select.spec.ts
@@ -3,7 +3,46 @@ describe('Stage select', () => {
     it('moves to stage 1', () => {
       cy.visit('/')
       cy.findByTestId('stage-select-panel-1').click()
-      cy.url().should('include', '/stages/1')
+      cy.url().should('eq', Cypress.config().baseUrl + '/stages/1')
+    })
+  })
+  
+  context('when the user clicks stage 2 panel', () => {
+    context('if stage 1 is not cleared yet', () => {
+      it('remains home', () => {
+        cy.visit('/')
+        cy.findByTestId('stage-select-panel-2').click()
+        cy.url().should('eq', Cypress.config().baseUrl + '/')
+      })
+    })
+    
+    context('if stage 1 is already cleared', () => {
+      it('moves to stage 2', () => {
+        cy.setLocalStorage('gorogoropanda.com/clearedStage', 'gjj');
+        cy.visit('/')
+        cy.findByTestId('stage-select-panel-2').click()
+        cy.url().should('eq', Cypress.config().baseUrl + '/stages/2')
+      })
+    })
+  })
+  
+  context('when the user clicks review stage 1 panel', () => {
+    context('if stage 9 is already cleared and stage 10 is not cleared yet', () => {
+      it('remains home', () => {
+        cy.setLocalStorage('gorogoropanda.com/clearedStage', 'u51');
+        cy.visit('/')
+        cy.findByTestId('review-panel-1').click()
+        cy.url().should('eq', Cypress.config().baseUrl + '/')
+      })
+    })
+    
+    context('if stage 10 is already cleared', () => {
+      it('moves to review 1', () => {
+        cy.setLocalStorage('gorogoropanda.com/clearedStage', 'ttw');
+        cy.visit('/')
+        cy.findByTestId('review-panel-1').click()
+        cy.url().should('eq', Cypress.config().baseUrl + '/reviews/1')
+      })
     })
   })
 })
